05a938ab74225d320c850f65f60ab37ccc972b2a
fuyutianji <1227797875@qq.com>
2026-08-11 16:27:45 +0800

    Embed Smart Collision in the Static Mesh Editor

--- Source/SmartCollisionEditor/Private/SmartCollisionEditorModule.cpp
+++ Source/SmartCollisionEditor/Private/SmartCollisionEditorModule.cpp
@@ -1,60 +1,133 @@
 #include "SmartCollisionEditorModule.h"
 
-#include "SSmartCollisionPanel.h"
-#include "Framework/Commands/UIAction.h"
 #include "Framework/Docking/TabManager.h"
-#include "ToolMenus.h"
+#include "Framework/MultiBox/MultiBoxBuilder.h"
+#include "IStaticMeshEditor.h"
+#include "SSmartCollisionPanel.h"
+#include "StaticMeshEditorModule.h"
+#include "Styling/AppStyle.h"
 #include "Widgets/Docking/SDockTab.h"
 
 #define LOCTEXT_NAMESPACE "FSmartCollisionEditorModule"
 
 namespace
 {
-    const FName SmartCollisionTabName(TEXT("SmartCollisionEditor"));
+    const FName SmartCollisionTabName(TEXT("StaticMeshEditor_SmartCollision"));
+
+    void OpenSmartCollisionTab(TWeakPtr<IStaticMeshEditor> WeakEditor)
+    {
+        if (const TSharedPtr<IStaticMeshEditor> Editor = WeakEditor.Pin())
+        {
+            if (const TSharedPtr<FTabManager> TabManager = Editor->GetTabManager())
+            {
+                TabManager->TryInvokeTab(SmartCollisionTabName);
+            }
+        }
+    }
 }
 
 void FSmartCollisionEditorModule::StartupModule()
 {
-    FGlobalTabmanager::Get()->RegisterNomadTabSpawner(
-        SmartCollisionTabName,
-        FOnSpawnTab::CreateLambda([](const FSpawnTabArgs&)
-        {
-            return SNew(SDockTab)
-                .TabRole(ETabRole::NomadTab)
-                [
-                    SNew(SSmartCollisionPanel)
-                ];
-        }))
-        .SetDisplayName(LOCTEXT("TabTitle", "Smart Collision Editor"))
-        .SetTooltipText(LOCTEXT("TabTooltip", "Generate collision for connected Static Mesh parts."));
+    IStaticMeshEditorModule& StaticMeshEditorModule =
+        FModuleManager::LoadModuleChecked<IStaticMeshEditorModule>(TEXT("StaticMeshEditor"));
 
-    UToolMenus::RegisterStartupCallback(
-        FSimpleMulticastDelegate::FDelegate::CreateRaw(this, &FSmartCollisionEditorModule::RegisterMenus));
+    EditorOpenedHandle = StaticMeshEditorModule.OnStaticMeshEditorOpened().AddRaw(
+        this,
+        &FSmartCollisionEditorModule::HandleStaticMeshEditorOpened);
+
+    ToolbarExtenderHandle =
+        StaticMeshEditorModule.GetAllStaticMeshEditorToolbarExtenders().Add(
+            IStaticMeshEditorModule::FStaticMeshEditorToolbarExtender::CreateRaw(
+                this,
+                &FSmartCollisionEditorModule::ExtendStaticMeshEditorToolbar));
 }
 
 void FSmartCollisionEditorModule::ShutdownModule()
 {
-    UToolMenus::UnRegisterStartupCallback(this);
-    UToolMenus::UnregisterOwner(this);
-    FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(SmartCollisionTabName);
+    if (!FModuleManager::Get().IsModuleLoaded(TEXT("StaticMeshEditor")))
+    {
+        return;
+    }
+
+    IStaticMeshEditorModule& StaticMeshEditorModule =
+        FModuleManager::GetModuleChecked<IStaticMeshEditorModule>(TEXT("StaticMeshEditor"));
+
+    StaticMeshEditorModule.OnStaticMeshEditorOpened().Remove(EditorOpenedHandle);
+    StaticMeshEditorModule.GetAllStaticMeshEditorToolbarExtenders().Remove(ToolbarExtenderHandle);
 }
 
-void FSmartCollisionEditorModule::RegisterMenus()
+void FSmartCollisionEditorModule::HandleStaticMeshEditorOpened(
+    TWeakPtr<IStaticMeshEditor> WeakEditor)
 {
-    FToolMenuOwnerScoped OwnerScoped(this);
+    const TSharedPtr<IStaticMeshEditor> Editor = WeakEditor.Pin();
+    if (!Editor)
+    {
+        return;
+    }
 
-    UToolMenu* Menu = UToolMenus::Get()->ExtendMenu(TEXT("LevelEditor.MainMenu.Tools"));
-    FToolMenuSection& Section = Menu->FindOrAddSection(TEXT("Tools"));
-
-    Section.AddMenuEntry(
-        TEXT("OpenSmartCollisionEditor"),
-        LOCTEXT("MenuLabel", "Smart Collision Editor"),
-        LOCTEXT("MenuTooltip", "Open the connected-part collision fitting tool."),
-        FSlateIcon(),
-        FUIAction(FExecuteAction::CreateLambda([]
+    Editor->OnStaticMeshEditorDockingExtentionTabs().AddLambda(
+        [](const TSharedRef<FTabManager::FStack>& ExtensionStack)
         {
-            FGlobalTabmanager::Get()->TryInvokeTab(SmartCollisionTabName);
-        })));
+            ExtensionStack->AddTab(SmartCollisionTabName, ETabState::OpenedTab);
+        });
+
+    Editor->OnRegisterTabSpawners().AddLambda(
+        [WeakEditor](const TSharedRef<FTabManager>& TabManager)
+        {
+            TabManager->RegisterTabSpawner(
+                SmartCollisionTabName,
+                FOnSpawnTab::CreateLambda(
+                    [WeakEditor](const FSpawnTabArgs&)
+                    {
+                        return SNew(SDockTab)
+                            .TabRole(ETabRole::PanelTab)
+                            [
+                                SNew(SSmartCollisionPanel)
+                                .StaticMeshEditor(WeakEditor)
+                            ];
+                    }))
+                .SetDisplayName(LOCTEXT("EmbeddedTabTitle", "Smart Collision"))
+                .SetTooltipText(LOCTEXT(
+                    "EmbeddedTabTooltip",
+                    "Select mesh faces or connected parts and fit collision."));
+        });
+
+    Editor->OnUnregisterTabSpawners().AddLambda(
+        [](const TSharedRef<FTabManager>& TabManager)
+        {
+            TabManager->UnregisterTabSpawner(SmartCollisionTabName);
+        });
+}
+
+TSharedRef<FExtender> FSmartCollisionEditorModule::ExtendStaticMeshEditorToolbar(
+    const TSharedRef<FUICommandList> CommandList,
+    TSharedRef<IStaticMeshEditor> Editor)
+{
+    const TSharedRef<FExtender> Extender = MakeShared<FExtender>();
+    const TWeakPtr<IStaticMeshEditor> WeakEditor = Editor;
+
+    Extender->AddToolBarExtension(
+        TEXT("Extensions"),
+        EExtensionHook::After,
+        CommandList,
+        FToolBarExtensionDelegate::CreateLambda(
+            [WeakEditor](FToolBarBuilder& ToolbarBuilder)
+            {
+                ToolbarBuilder.AddToolBarButton(
+                    FUIAction(FExecuteAction::CreateStatic(
+                        &OpenSmartCollisionTab,
+                        WeakEditor)),
+                    NAME_None,
+                    LOCTEXT("ToolbarLabel", "Smart Collision"),
+                    LOCTEXT(
+                        "ToolbarTooltip",
+                        "Open interactive face and connected-part collision fitting."),
+                    FSlateIcon(
+                        FAppStyle::GetAppStyleSetName(),
+                        TEXT("StaticMeshEditor.SetShowCollision")));
+            }));
+
+    return Extender;
 }
 
 IMPLEMENT_MODULE(FSmartCollisionEditorModule, SmartCollisionEditor)
