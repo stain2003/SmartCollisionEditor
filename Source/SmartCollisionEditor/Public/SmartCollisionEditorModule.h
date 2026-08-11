e1781fde6f286ce6c30bccaf28a809d0906c0aea
fuyutianji <1227797875@qq.com>
2026-08-11 16:27:43 +0800

    Embed Smart Collision in the Static Mesh Editor

--- Source/SmartCollisionEditor/Public/SmartCollisionEditorModule.h
+++ Source/SmartCollisionEditor/Public/SmartCollisionEditorModule.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
+
+class FExtender;
+class FTabManager;
+class FUICommandList;
+class IStaticMeshEditor;
 
 class FSmartCollisionEditorModule final : public IModuleInterface
 {
@@ -9,5 +15,11 @@ public:
     virtual void ShutdownModule() override;
 
 private:
-    void RegisterMenus();
+    void HandleStaticMeshEditorOpened(TWeakPtr<IStaticMeshEditor> Editor);
+    TSharedRef<FExtender> ExtendStaticMeshEditorToolbar(
+        const TSharedRef<FUICommandList> CommandList,
+        TSharedRef<IStaticMeshEditor> Editor);
+
+    FDelegateHandle EditorOpenedHandle;
+    FDelegateHandle ToolbarExtenderHandle;
 };
